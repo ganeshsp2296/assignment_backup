c9764dd0a080fbe68adbbb473ec0268b4de178cd
Ubuntu <ubuntu@ip-172-31-38-167.ap-south-1.compute.internal>
2025-05-03 11:27:30 +0000

    pushing to git repo

---
+++ backup_and_audit1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+repo_list_file="git_url.txt"
+backup_dir="/home/ubuntu/backup1"
+today=$(date +%Y%m%d)
+
+mkdir -p "$backup_dir"
+
+while read -r repo_url; do
+    repo_name=$(basename "$repo_url" .git)
+    repo_path="$backup_dir/$repo_name"
+
+    if [ ! -d "$repo_path/.git" ]; then
+        echo "Cloning $repo_name..."
+        git clone "$repo_url" "$repo_path"
+    else
+        echo "Updating $repo_name..."
+        cd "$repo_path" && git pull
+    fi
+
+
+echo "Creating a tar backup for $repo_name"
+tar -czvf "$backup_dir/${repo_name}-${today}.tar.gz" -C "$backup_dir" "$repo_name"
+
+echo "Searching for recent commits for $repo_name"
+cd "$repo_path"
+git log --since=1.day > "$backup_dir/audit-${repo_name}-${today}.txt"
+
+done < "$repo_list_file"
