c111e0b7196fc0c224938a814044c82af4723e2c
Ubuntu <ubuntu@ip-172-31-38-167.ap-south-1.compute.internal>
2025-05-03 13:48:42 +0000

    changes

--- backup_and_audit1.sh
+++ backup_and_audit1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 repo_list_file="git_url.txt"
-backup_dir="/var/backups/git-repos"
+backup_dir="$WORKSPACE/backups"
 today=$(date +%Y%m%d)
 
 mkdir -p "$backup_dir"
@@ -16,20 +16,18 @@ while read -r repo_url; do
     else
         echo "Updating $repo_name..."
         cd "$repo_path" || continue
-	sudo chown -R "$(whoami)":"$(whoami)" "$repo_path"
-        sudo chmod -R u+rwX "$repo_path"
-	git config --global --add safe.directory "$repo_path"
-	git reset --hard HEAD
-	git clean -fd
-	git pull
+        git config --global --add safe.directory "$repo_path"
+        git reset --hard HEAD
+        git clean -fd
+        git pull
     fi
 
+    echo "Creating a tar backup for $repo_name"
+    tar -czvf "$backup_dir/${repo_name}-${today}.tar.gz" -C "$backup_dir" "$repo_name"
 
-echo "Creating a tar backup for $repo_name"
-tar -czvf "$backup_dir/${repo_name}-${today}.tar.gz" -C "$backup_dir" "$repo_name"
-
-echo "Searching for recent commits for $repo_name"
-cd "$repo_path" || continue
-git log --since=1.day > "$backup_dir/audit-${repo_name}-${today}.txt"
+    echo "Searching for recent commits for $repo_name"
+    cd "$repo_path" || continue
+    git log --since=1.day > "$backup_dir/audit-${repo_name}-${today}.txt"
 
 done < "$repo_list_file"
+
